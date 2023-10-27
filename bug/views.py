ff3abe4040ee1d62f57f1a881b2a77712253b661
piotr bronikowski <pbrqsl@gmail.com>
2023-10-27 19:02:16 +0200

    Add GET method only

--- bug/views.py
+++ bug/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_list_or_404, render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import generics
+from django.views.decorators.http import require_GET
 
 
-from bug.models import Bug, User
+from bug.models import Bug, User, Project
 from bug.serializers import BugsSerializer, UserSerializer
 
 
@@ -16,29 +16,35 @@ class UserApiView(generics.ListAPIView):
 class BugsApiView(generics.ListAPIView):
     serializer_class = BugsSerializer
     queryset = Bug.objects.all()
-
+    http_method_names = ['get']
     project_id = None
     user_id = None
+
+    def user_exists(self, id):
+        return User.objects.filter(id=id).exists()
+
+    def project_exists(self, id):
+        return Project.objects.filter(id=id).exists()
 
     def get(self, request, *args, **kwargs):
         print(self.request.GET.get("project_id"))
         self.project_id = self.request.GET.get("project_id")
         self.user_id = self.request.GET.get("user_id")
-        if self.project_id or self.user_id:
-            if self.project_id:
-                self.queryset = self.queryset.filter(project=self.project_id)
-                print(len(self.queryset))
-            if self.user_id:
-                self.queryset = self.queryset.filter(user=self.user_id)
-            if len(self.queryset) == 0:
+        if self.project_id:
+            if not self.project_exists(self.project_id):
+                print('project does not exit')
                 return Response(status=status.HTTP_404_NOT_FOUND)
+            self.queryset = self.queryset.filter(project=self.project_id)
+            
+        if self.user_id:
+            if not self.user_exists(self.user_id):
+                print('user does not exit')
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            self.queryset = self.queryset.filter(user=self.user_id)    
             return super().get(request, *args, **kwargs)
         self.queryset = self.queryset.none()
         return super().get(request, *args, **kwargs)
 
-    def get_queryset(self):
-        print(self.project_id)
-        return super().get_queryset()
 
 
-# Create your views here.
+
